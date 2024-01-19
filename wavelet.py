50452ea0b8dede7fed599a1442bcd86ecc2d5386
lakshitamohanraj <lakshitamohanraj20@gmail.com>
2024-01-19 12:19:59 +0530

    ready to deploy

---
+++ wavelet.py
@@ -0,0 +1,25 @@
+import pywt
+import numpy as np
+import cv2
+
+def w2d(img,mode="haar",level=1):
+    imArray=img
+    
+    #convert to grayScale
+    imArray=cv2.cvtColor(imArray,cv2.COLOR_RGB2GRAY)
+    
+    #convert to float
+    imArray=np.float32(imArray)
+    imArray/=255
+    #compute Cofficients
+    coeffs=pywt.wavedec2(imArray,mode,level=level)
+    
+    #process cofficients
+    coeffs_H=list(coeffs)
+    coeffs_H[0]*=0
+    
+    imArray_H=pywt.waverec2(coeffs_H,mode)
+    imArray_H*=255
+    imArray_H=np.uint8(imArray_H)
+    
+    return imArray_H
